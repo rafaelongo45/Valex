bdcfb7b8c9e18e050e427bed6f271c394364023c
Rafael Longo <rafaelongo45@gmail.com>
2022-07-10 13:44:10 -0300

    Feat: Implements validation to check if employee works in the company that has the sent api key

--- Controllers/rechargeController.ts
+++ Controllers/rechargeController.ts
@@ -4,7 +4,8 @@ import * as rechargeService from "../Services/rechargeService.js";
 
 export async function rechargeCard(req: Request, res: Response){
   const { id } = req.params;
+  const { key } = req.headers;
   const { amount } : { amount: number }= req.body;
-  await rechargeService.recharge(id, amount);
+  await rechargeService.recharge(id, amount, key);
   return res.sendStatus(200);
 };
--- Services/rechargeService.ts
+++ Services/rechargeService.ts
@@ -1,6 +1,8 @@
-import * as cardRepository from "../repositories/cardRepository.js";
-import * as rechargeRepository from "../repositories/rechargeRepository.js";
 import * as cardService from "../Services/cardService.js";
+import * as cardRepository from "../repositories/cardRepository.js";
+import * as employeeRepository from "../repositories/employeeRepository.js";
+import * as rechargeRepository from "../repositories/rechargeRepository.js";
+import * as companyRepository from "../repositories/companyRepository.js";
 
 export function isAmountValid(amount: number){
   const validAmount = amount && amount > 0
@@ -26,10 +28,23 @@ async function insertAmount(cardId: number, amount: number){
   await rechargeRepository.insert(rechargeData)
 };
 
-export async function recharge(cardId, amount: number){
+async function isEmployeeFromCompany(cardId: number, apiKey){
+  const cardDetails = await cardRepository.findById(cardId);
+  const employeeDetails = await employeeRepository.findById(cardDetails.employeeId);
+  const companyDetails = await companyRepository.findByApiKey(apiKey);
+  const isFromCompany = companyDetails.id === employeeDetails.companyId
+
+  if(!isFromCompany){
+    throw { type: "rechargeError", message: "Employee doesn't belong to the company", code: 403}
+  }
+};
+
+
+export async function recharge(cardId, amount: number, key){
   isAmountValid(amount);
   await cardService.cardExists(cardId);
   await isCardActive(cardId);
   await cardService.isCardExpired(cardId);
+  await isEmployeeFromCompany(cardId, key);
   await insertAmount(cardId, amount);
 };
