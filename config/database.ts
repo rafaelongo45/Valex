c82dbcc9930fc702d70486e599309e4f471cf6d9
Rafael Longo <rafaelongo45@gmail.com>
2022-07-11 13:58:10 -0300

    Preparing for deploy

--- config/database.ts
+++ config/database.ts
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
-import pg from "pg";
+import pg from 'pg';
+
 dotenv.config();
 
 const { Pool } = pg;
-export const connection = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
+const connection = new Pool(databaseConfig);
+
+export default connection;
